ff4dde483b52ed23329b670cc049fb2a2fdd07fd
Mike Slinn <mslinn@mslinn.com>
2024-03-13 12:03:04 -0400

    -

--- lib/live_set.rb
+++ lib/live_set.rb
@@ -1,3 +1,4 @@
+require 'nokogiri'
 require 'zlib'
 require_relative 'live_set/version'
 
@@ -18,6 +19,10 @@ set_name = ARGV.join ' '
 help "#{set_name} does not exist" unless File.exist? set_name
 help "#{set_name} is a directory" if File.directory? set_name
 
-contents = Zlib::GzipReader.open(set_name) { |gz| gz.readlines }
+contents = Zlib::GzipReader.open(set_name, &:readlines)
 xml_header = puts contents[1]
 puts xml_header
+
+xml_doc = Nokogiri::XML contents.join('\n')
+thing = xml_doc.at_xpath('//things')
+puts thing
